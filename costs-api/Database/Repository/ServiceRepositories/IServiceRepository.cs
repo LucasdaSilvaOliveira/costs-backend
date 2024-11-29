dfd752e61c01027f50f4b72769c12f6788cf3d8e
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2024-11-29 14:21:50 -0300

    feat: Implementação do repositorio da tabela Service

--- costs-api/Database/Repository/ServiceRepositories/IServiceRepository.cs
+++ costs-api/Database/Repository/ServiceRepositories/IServiceRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace costs_api.Database.Repository.ServiceRepositories
+﻿using costs_api.Database.Entities;
+
+namespace costs_api.Database.Repository.ServiceRepositories
 {
     public interface IServiceRepository
     {
+        Task<List<Service>> GetAll();
+        Task<Service> GetById(int id);
+        Task<bool> Update(Service project);
+        Task<bool> Delete(Service project);
+        Task<bool> Add(Service project);
     }
 }
--- costs-api/Database/Repository/ServiceRepositories/ServiceRepository.cs
+++ costs-api/Database/Repository/ServiceRepositories/ServiceRepository.cs
@@ -1,6 +1,47 @@
-﻿namespace costs_api.Database.Repository.ServiceRepositories
+﻿using costs_api.Database.Context;
+using costs_api.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace costs_api.Database.Repository.ServiceRepositories
 {
     public class ServiceRepository : IServiceRepository
     {
+        private readonly BancoContext _context;
+        public ServiceRepository(BancoContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Add(Service service)
+        {
+            await _context.Services.AddAsync(service);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Delete(Service service)
+        {
+            _context.Services.Remove(service);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<List<Service>> GetAll()
+        {
+            var services = await _context.Services.ToListAsync();
+            return services;
+        }
+
+        public async Task<Service> GetById(int id)
+        {
+            var service = await _context.Services.FirstOrDefaultAsync(x => x.Id == id);
+            return service;
+        }
+
+        public async Task<bool> Update(Service service)
+        {
+            _context.Services.Update(service);
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 }
