c4441808187ee1047e69875d4b926af4b97c1515
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2024-11-29 17:58:53 -0300

    feat: add controllers

---
+++ costs-api/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using costs_api.Database.Repository.ProjectRepositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace costs_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectRepository _repository;
+        public ProjectController(IProjectRepository repository)
+        {
+            _repository = repository;
+        }
+
+        //public async Task<IActionResult> GetAllProjects()
+        //{
+        //    var projects = await _repository.GetAll();
+        //}
+    }
+}
