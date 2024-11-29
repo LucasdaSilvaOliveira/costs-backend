581cbbc2d34ac7e1d211df3c7a6b02111c8e8dcb
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2024-11-29 19:17:08 -0300

    feat: add uptade and add method in projectcontroller

--- costs-api/Controllers/ProjectController.cs
+++ costs-api/Controllers/ProjectController.cs
@@ -1,4 +1,7 @@
-﻿using costs_api.Database.Repository.ProjectRepositories;
+﻿using AutoMapper;
+using costs_api.Database.Entities;
+using costs_api.Database.Repository.ProjectRepositories;
+using costs_api.DTOs.Project;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,14 +12,48 @@ namespace costs_api.Controllers
     public class ProjectController : ControllerBase
     {
         private readonly IProjectRepository _repository;
-        public ProjectController(IProjectRepository repository)
+        private readonly IMapper _mapper;
+        public ProjectController(IProjectRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
-        //public async Task<IActionResult> GetAllProjects()
-        //{
-        //    var projects = await _repository.GetAll();
-        //}
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var projects = await _repository.GetAll();
+
+            var dtos = _mapper.Map<List<ProjectDTO>>(projects);
+
+            return Ok(dtos);
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add(ProjectDTO dto)
+        {
+            if (dto == null) return BadRequest();
+
+            var project = _mapper.Map<Project>(dto);
+            await _repository.Add(project);
+
+            return Created();
+        }
+
+        [HttpPut("Update/{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody]ProjectDTO dto)
+        {
+            var project = await _repository.GetById(id);
+
+            if (project == null || dto == null) return BadRequest();
+
+            project.Name = dto.Name;
+            project.Budget = dto.Budget;
+            project.Category = dto.Category;
+
+            await _repository.Update(project);
+
+            return Ok();
+        }
     }
 }
