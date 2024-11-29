e5780818f51e8e6f6c17658f6a8e671ee69f0a3f
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2024-11-29 17:59:05 -0300

    feat: add DTOs

---
+++ costs-api/DTOs/Project/ProjectDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace costs_api.DTOs.Project
+{
+    public class ProjectDTO
+    {
+        public string Name { get; set; }
+        public decimal Budget { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ costs-api/DTOs/Service/ServiceDTO.cs
@@ -0,0 +1,11 @@
+﻿
+namespace costs_api.DTOs.Service
+{
+    public class ServiceDTO
+    {
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+        public string ProjectName { get; set; }
+    }
+}
