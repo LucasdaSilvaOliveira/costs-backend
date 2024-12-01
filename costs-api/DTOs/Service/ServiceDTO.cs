1aac2f0f61fef787ce725d5629531098673ec94a
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2024-12-01 14:27:17 -0300

    fix: add id colum in ServiceDTO

--- costs-api/DTOs/Service/ServiceDTO.cs
+++ costs-api/DTOs/Service/ServiceDTO.cs
@@ -3,6 +3,7 @@ namespace costs_api.DTOs.Service
 {
     public class ServiceDTO
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Cost { get; set; }
         public string Description { get; set; }
